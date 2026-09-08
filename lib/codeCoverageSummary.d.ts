49e3c3f1b1f1b3e88547658e99e2c88e8273b605
David Buzinski <davidb@d13-dev.dhcp.mathworks.com>
2026-09-08 11:26:47 -0400

    built

---
+++ lib/codeCoverageSummary.d.ts
@@ -0,0 +1,15 @@
+interface CoverageMetric {
+    Executed: number;
+    Total: number;
+    Percentage: number;
+}
+export interface CoverageData {
+    FunctionCoverage?: CoverageMetric;
+    StatementCoverage?: CoverageMetric;
+    DecisionCoverage?: CoverageMetric;
+    ConditionCoverage?: CoverageMetric;
+    MCDCCoverage?: CoverageMetric;
+}
+export declare function getCoverageResults(runnerTemp: string, runId: string): CoverageData | null;
+export declare function getCoverageTable(coverage: CoverageData): string;
+export {};
